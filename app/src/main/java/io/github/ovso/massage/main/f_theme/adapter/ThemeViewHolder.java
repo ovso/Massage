4e831b0f89afc49fd18d4799c8bf1df806d72e2b
peterjaeho <peter@mybroccoli.co.kr>
2020-02-05 05:36:52 +0900

    Remove favorite feature

--- app/src/main/java/io/github/ovso/massage/main/f_theme/adapter/ThemeViewHolder.java
+++ app/src/main/java/io/github/ovso/massage/main/f_theme/adapter/ThemeViewHolder.java
@@ -12,7 +12,6 @@ public class ThemeViewHolder extends BaseRecyclerAdapter.BaseViewHolder {
 
   @BindView(R.id.title_textview) TextView titleTextview;
   @BindView(R.id.video_button) ImageButton videoButton;
-  @BindView(R.id.fav_button) ImageButton favButton;
 
   public ThemeViewHolder(View itemView) {
     super(itemView);
