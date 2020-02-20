44463083f8363571a21727703137e5c4b6f6c250
peterjaeho <peter@mybroccoli.co.kr>
2020-02-20 09:03:49 +0900

    Sorting

--- app/src/main/java/io/github/ovso/massage/view/ui/player/PortraitVideoActivity.java
+++ app/src/main/java/io/github/ovso/massage/view/ui/player/PortraitVideoActivity.java
@@ -3,37 +3,43 @@ package io.github.ovso.massage.view.ui.player;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.widget.Toast;
+
 import butterknife.ButterKnife;
+
 import com.google.android.youtube.player.YouTubeInitializationResult;
 import com.google.android.youtube.player.YouTubePlayer;
 import com.google.android.youtube.player.YouTubePlayerFragment;
+
 import io.github.ovso.massage.R;
 import io.github.ovso.massage.Security;
 import io.github.ovso.massage.ad.AdsActivity;
 
 public class PortraitVideoActivity extends AdsActivity {
 
-    @Override protected void onCreate(@Nullable Bundle savedInstanceState) {
-      super.onCreate(savedInstanceState);
-      setContentView(R.layout.activity_fullscreen_video);
-      ButterKnife.bind(this);
-      if (getIntent().hasExtra("video_id")) {
-        YouTubePlayerFragment youTubePlayerFragment =
-            (YouTubePlayerFragment) getFragmentManager().findFragmentById(R.id.youtube_fragment);
-        youTubePlayerFragment.initialize(Security.YOUTUBE_DEVELOPER_KEY.getValue(),
-            new YouTubePlayer.OnInitializedListener() {
-              @Override public void onInitializationSuccess(YouTubePlayer.Provider provider,
-                  YouTubePlayer youTubePlayer, boolean b) {
-                youTubePlayer.loadVideo(getIntent().getStringExtra("video_id"));
-              }
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fullscreen_video);
+        ButterKnife.bind(this);
+        if (getIntent().hasExtra("video_id")) {
+            YouTubePlayerFragment youTubePlayerFragment =
+                    (YouTubePlayerFragment) getFragmentManager().findFragmentById(R.id.youtube_fragment);
+            youTubePlayerFragment.initialize(Security.YOUTUBE_DEVELOPER_KEY.getValue(),
+                    new YouTubePlayer.OnInitializedListener() {
+                        @Override
+                        public void onInitializationSuccess(YouTubePlayer.Provider provider,
+                                                            YouTubePlayer youTubePlayer, boolean b) {
+                            youTubePlayer.loadVideo(getIntent().getStringExtra("video_id"));
+                        }
 
-              @Override public void onInitializationFailure(YouTubePlayer.Provider provider,
-                  YouTubeInitializationResult youTubeInitializationResult) {
-              }
-            });
-      } else {
-        Toast.makeText(this, R.string.no_videos_found, Toast.LENGTH_SHORT).show();
-        finish();
-      }
+                        @Override
+                        public void onInitializationFailure(YouTubePlayer.Provider provider,
+                                                            YouTubeInitializationResult youTubeInitializationResult) {
+                        }
+                    });
+        } else {
+            Toast.makeText(this, R.string.no_videos_found, Toast.LENGTH_SHORT).show();
+            finish();
+        }
     }
-  }
+}
