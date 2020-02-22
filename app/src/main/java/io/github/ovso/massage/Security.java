ec7775c2d47538daab09b3578188661363a67143
peterjaeho <peter@mybroccoli.co.kr>
2020-02-22 19:56:08 +0900

    Remote cauly code

--- app/src/main/java/io/github/ovso/massage/Security.java
+++ app/src/main/java/io/github/ovso/massage/Security.java
@@ -2,10 +2,6 @@ package io.github.ovso.massage;
 
 import lombok.Getter;
 
-/**
- * Created by jaeho on 2018. 1. 25
- */
-
 public enum Security {
   /*
   CLIENT_ID("WrYoNbUDp88vf6SG18ZL"), CLIENT_SECRET("vx6fv5kovK"), API_BASE_URL(
@@ -18,8 +14,6 @@ public enum Security {
 
   YOUTUBE_DEVELOPER_KEY(
       "AIzaSyBdY9vP4_vQs5YEGJ3Ghu6s5gGY8yFlo0s"),
-
-  CAULY_APP_CODE("AZYsWa4X"),
 
   ADMOB_APP_ID(App.isDebug() ? "ca-app-pub-3940256099942544~3347511713"
       : "ca-app-pub-8679189423397017~4275586930"),
@@ -34,7 +28,7 @@ public enum Security {
 
   @Getter private String value;
 
-  private Security(String value) {
+  Security(String value) {
     this.value = value;
   }
 }
