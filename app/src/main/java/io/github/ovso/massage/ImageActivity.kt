0d2af1e8c422debfeb47597ccd3ac259e1df6d61
jaeho <ovsoce@gmail.com>
2020-02-26 23:44:01 +0900

    Insert toolbar, Apply support action bar

--- app/src/main/java/io/github/ovso/massage/ImageActivity.kt
+++ app/src/main/java/io/github/ovso/massage/ImageActivity.kt
@@ -2,6 +2,7 @@ package io.github.ovso.massage
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.activity_image.*
 
@@ -9,9 +10,17 @@ class ImageActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        title = getString(R.string.acupressure_point)
         setContentView(R.layout.activity_image)
+        setSupportActionBar(toolbar_image)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
         Glide.with(photo_view_image)
             .load(intent.getStringExtra("image_url")).into(photo_view_image)
         tv_image.text = intent.getStringExtra("site")
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        finish()
+        return super.onOptionsItemSelected(item)
     }
 }
