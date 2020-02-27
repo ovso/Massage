28ea3ce9691c5a1d99aef0c71e81fed1a618b96a
jaeho <ovsoce@gmail.com>
2020-02-27 23:04:22 +0900

    Apply ViewBinding, Added Banner ads

--- app/src/main/java/io/github/ovso/massage/ImageActivity.kt
+++ app/src/main/java/io/github/ovso/massage/ImageActivity.kt
@@ -7,24 +7,36 @@ import com.bumptech.glide.Glide
 import com.google.android.gms.ads.AdListener
 import com.google.android.gms.ads.AdRequest
 import com.google.android.gms.ads.InterstitialAd
-import io.github.ovso.massage.framework.ObjectUtils
+import io.github.ovso.massage.ad.MyAdView
+import io.github.ovso.massage.databinding.ActivityImageBinding
 import kotlinx.android.synthetic.main.activity_image.*
 
 class ImageActivity : AppCompatActivity() {
 
     private lateinit var interstitialAd: InterstitialAd
 
+    private lateinit var binding: ActivityImageBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setupInterstitialAd()
-
+        binding = ActivityImageBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         title = getString(R.string.acupressure_point)
-        setContentView(R.layout.activity_image)
-        setSupportActionBar(toolbar_image)
+        initAds()
+        setSupportActionBar(binding.toolbarImage)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         Glide.with(photo_view_image)
             .load(intent.getStringExtra("image_url")).into(photo_view_image)
         tv_image.text = intent.getStringExtra("site")
+    }
+
+    private fun initAds() {
+        setupInterstitialAd()
+        setupBannerAd()
+    }
+
+    private fun setupBannerAd() {
+        binding.adContainer.addView(MyAdView.getAdmobAdView(applicationContext))
     }
 
     private fun setupInterstitialAd() {
@@ -57,12 +69,10 @@ class ImageActivity : AppCompatActivity() {
     }
 
     private fun showInterstitialAd() {
-        if (!ObjectUtils.isEmpty(interstitialAd)) {
-            if (interstitialAd.isLoaded) {
-                interstitialAd.show()
-            } else {
-                finish()
-            }
+        if (interstitialAd.isLoaded) {
+            interstitialAd.show()
+        } else {
+            finish()
         }
     }
 }
