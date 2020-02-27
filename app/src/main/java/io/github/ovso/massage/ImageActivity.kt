b1337acd7747657413ce2675e31e6cee51b509fc
jaeho <ovsoce@gmail.com>
2020-02-27 22:41:48 +0900

    Implement interstital ad on ImageActivity

--- app/src/main/java/io/github/ovso/massage/ImageActivity.kt
+++ app/src/main/java/io/github/ovso/massage/ImageActivity.kt
@@ -1,15 +1,23 @@
 package io.github.ovso.massage
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
+import com.google.android.gms.ads.AdListener
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.InterstitialAd
+import io.github.ovso.massage.framework.ObjectUtils
 import kotlinx.android.synthetic.main.activity_image.*
 
 class ImageActivity : AppCompatActivity() {
 
+    private lateinit var interstitialAd: InterstitialAd
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setupInterstitialAd()
+
         title = getString(R.string.acupressure_point)
         setContentView(R.layout.activity_image)
         setSupportActionBar(toolbar_image)
@@ -19,8 +27,42 @@ class ImageActivity : AppCompatActivity() {
         tv_image.text = intent.getStringExtra("site")
     }
 
+    private fun setupInterstitialAd() {
+        interstitialAd = provideInterstitialAd()
+        interstitialAd.adListener = interstitialAdListener
+    }
+
+    private val interstitialAdListener: AdListener = object : AdListener() {
+        override fun onAdClosed() {
+            super.onAdClosed()
+            showInterstitialAd()
+        }
+    }
+
+    private fun provideInterstitialAd() =
+        InterstitialAd(applicationContext).apply {
+            adUnitId = getString(R.string.ads_interstitial_unit_id)
+            val adRequestBuilder =
+                AdRequest.Builder()
+            loadAd(adRequestBuilder.build())
+        }
+
     override fun onOptionsItemSelected(item: MenuItem?): Boolean {
-        finish()
+        showInterstitialAd()
         return super.onOptionsItemSelected(item)
+    }
+
+    override fun onBackPressed() {
+        showInterstitialAd()
+    }
+
+    private fun showInterstitialAd() {
+        if (!ObjectUtils.isEmpty(interstitialAd)) {
+            if (interstitialAd.isLoaded) {
+                interstitialAd.show()
+            } else {
+                finish()
+            }
+        }
     }
 }
