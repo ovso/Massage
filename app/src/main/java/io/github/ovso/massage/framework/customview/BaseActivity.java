5f8fcf80c9aba72eda1268090ff54ea250020874
jaeho <ovsoce@gmail.com>
2020-11-11 22:47:31 +0900

    feat: Converting java to kotlin

--- app/src/main/java/io/github/ovso/massage/framework/customview/BaseActivity.java
+++ app/src/main/java/io/github/ovso/massage/framework/customview/BaseActivity.java
@@ -8,43 +8,35 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.content.ContextCompat;
 
-import butterknife.BindView;
-import butterknife.ButterKnife;
-import butterknife.Unbinder;
 import io.github.ovso.massage.R;
 
-/**
- * Created by jaeho on 2017. 10. 19
- */
-
 public abstract class BaseActivity extends AppCompatActivity {
-  private Unbinder unbinder;
-  protected @BindView(R.id.toolbar)
-  Toolbar toolbar;
+    Toolbar toolbar;
 
-  @Override protected void onCreate(@Nullable Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(getLayoutResId());
-    unbinder = ButterKnife.bind(this);
-    setSupportActionBar(toolbar);
-    setNavigationBarColor();
-    onCreated(savedInstanceState);
-  }
-
-  private void setNavigationBarColor() {
-    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-      getWindow().setNavigationBarColor(ContextCompat.getColor(this, R.color.colorPrimary));
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(getLayoutResId());
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        setNavigationBarColor();
+        onCreated(savedInstanceState);
     }
-  }
 
-  @Override protected void onDestroy() {
-    super.onDestroy();
-    unbinder.unbind();
-  }
+    private void setNavigationBarColor() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            getWindow().setNavigationBarColor(ContextCompat.getColor(this, R.color.colorPrimary));
+        }
+    }
 
-  protected abstract int getLayoutResId();
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
 
-  protected void onCreated(Bundle savedInstanceState) {
+    protected abstract int getLayoutResId();
 
-  }
+    protected void onCreated(Bundle savedInstanceState) {
+
+    }
 }
